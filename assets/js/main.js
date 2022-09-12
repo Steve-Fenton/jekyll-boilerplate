aa4b1d144a37b650e0a830fde27f33a20957f591
Steve Fenton <99181436+steve-fenton-octopus@users.noreply.github.com>
2022-09-12 13:58:45 +0100

    Accessible navigation. Modal and focus trap

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,6 +62,25 @@ addResizedEventUtility(window, document);
 }
 
 /**
+ * Gets first, last, and all focusable elements in the target
+ * @param {HTMLElement} target element
+ * @returns {{first: HTMLElement, last: HTMLElement, all: HTMLElement[]}}}
+ */
+function getFocusableElement(target) {
+    var focusElements = Array.from(
+        qsa('a[href], button, input, textarea, select, details,[tabindex]:not([tabindex="-1"])', target)
+    ).filter(function(el) {
+        return !el.hasAttribute('disabled') && !el.getAttribute('aria-hidden');
+    });
+    
+    return {
+        first: focusElements[0],
+        last: focusElements[focusElements.length -1],
+        all: focusElements
+    };
+}
+
+/**
  * Makes an existing navigation element sticky
  * if the screen size makes that useful.
  * @param {string} headerSelector 
@@ -138,11 +157,45 @@ function addMobileNavigation(iconSelector, navigationSelector) {
     var overlay = document.createElement('div');
     var dataOpen = 'data-open';
 
+    icon.addEventListener('keydown', function(e) { 
+        if (icon.getAttribute(dataOpen) === dataOpen) {
+            var focusElements = getFocusableElement(overlay);
+            trapFocus(e, focusElements.first); 
+            trapReverseFocus(e, focusElements.last);
+        }
+    });
+
     function handleIconInteraction() {
         if (icon.dataset.open == dataOpen) {
             closeMobileMenu();
         } else {
             openMobileMenu();
+        }
+    }
+
+    function trapFocus(event, focusItem) {
+        if (event.shiftKey) {
+            return;
+        }
+
+        switch (event.code.toLowerCase()) {
+            case 'tab':
+            event.preventDefault();    
+            focusItem.focus();
+            return false;
+        }
+    }
+
+    function trapReverseFocus(event, focusItem) {
+        if (!event.shiftKey) {
+            return;
+        }
+
+        switch (event.code.toLowerCase()) {
+            case 'tab':
+            event.preventDefault();    
+            focusItem.focus();
+            return false;
         }
     }
 
@@ -153,6 +206,25 @@ function addMobileNavigation(iconSelector, navigationSelector) {
         overlay.innerHTML = navigation.outerHTML;
         overlay.className = 'overlay';
         overlay.style.display = 'block';
+
+        // Modal Accessibility
+        var title = qs('.site-nav-title', overlay);
+        title.setAttribute('id', 'modal-title');
+        title.setAttribute('tabindex', '-1');
+        overlay.setAttribute('role', 'dialog');
+        overlay.setAttribute('aria-modal', 'true');
+        overlay.setAttribute('aria-labelled-by', 'modal-title');
+
+        // Trap Focus to Visible Overlay
+        var focusElements = getFocusableElement(overlay);
+
+        focusElements.first.addEventListener('keydown', function(e) {
+            trapReverseFocus(e, icon);
+        })
+        focusElements.last.addEventListener('keydown', function(e) { 
+            trapFocus(e, icon); 
+        });
+
         icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" 
             width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" 
             fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -160,17 +232,19 @@ function addMobileNavigation(iconSelector, navigationSelector) {
             <line x1="18" y1="6" x2="6" y2="18" />
             <line x1="6" y1="6" x2="18" y2="18" />
             </svg>`;
-        document.body.appendChild(overlay);
 
-        qs('a', overlay).focus();
-        
+        document.body.appendChild(overlay);
         icon.setAttribute(dataOpen, dataOpen);
+        title.focus();
     }
 
     function closeMobileMenu() {
         document.body.style.overflow = 'auto';
+
         overlay.innerHTML = '';
         overlay.style.display = 'none';
+        document.body.removeChild(overlay);
+
         icon.innerHTML = originalIcon;
         icon.removeAttribute(dataOpen);
     }
