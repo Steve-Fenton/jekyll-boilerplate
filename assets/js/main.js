eb990b836a081c0bd8237f63cc6d03f9b739d762
Steve Fenton <99181436+steve-fenton-octopus@users.noreply.github.com>
2022-09-16 09:18:31 +0100

    Move "open" logic into Jekyll.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 // @ts-check
 
-import { setNavigationTree } from './modules/nav-expand.js';
 import { setClickableBlocks } from './modules/click-blocks.js';
 import { addResizedEvent } from './modules/resizing.js';
 import { addStickyNavigation } from './modules/nav-sticky.js';
@@ -8,11 +7,9 @@ import { addMobileNavigation } from './modules/nav-mobile.js';
 import { addIntersectionObserver } from './modules/animation.js';
 import { qsa } from './modules/query.js';
 
-setNavigationTree('details.sub-nav');
-setClickableBlocks();
-
 const resizedEventName = addResizedEvent();
 
+setClickableBlocks();
 addStickyNavigation('.site-header', '.site-nav', '.site-nav > ul', resizedEventName);
 addMobileNavigation('.navigation-icon', '.site-nav', resizedEventName);
 addIntersectionObserver('.post-list .list-item, main img, main .note, main blockquote');
